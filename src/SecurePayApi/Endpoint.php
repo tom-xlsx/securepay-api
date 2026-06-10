575e8698370d1a1ecb6761b28e61477e94c939b5
Tom Penney <tom.penney@fatzebra.com>
2026-06-10 16:24:32 +1000

    Update sanbox auth URL to the latest endpoint

--- src/SecurePayApi/Endpoint.php
+++ src/SecurePayApi/Endpoint.php
@@ -6,7 +6,7 @@ class Endpoint
 {
     public const ENDPOINT_API_SANDBOX = 'https://payments-stest.npe.auspost.zone';
     public const ENDPOINT_API_LIVE = 'https://payments.auspost.net.au';
-    public const ENDPOINT_AUTH_SANDBOX = 'https://welcome.api2.sandbox.auspost.com.au/oauth/token';
+    public const ENDPOINT_AUTH_SANDBOX = 'https://auth.sandbox.spa.pmnts.io/oauth/token';
     public const ENDPOINT_AUTH_LIVE = 'https://welcome.api2.auspost.com.au/oauth/token';
     public const URL_SANDBOX_SCRIPT = 'https://payments-stest.npe.auspost.zone/v3/ui/client/securepay-ui.min.js';
     public const URL_LIVE_SCRIPT = 'https://payments.auspost.net.au/v3/ui/client/securepay-ui.min.js';
